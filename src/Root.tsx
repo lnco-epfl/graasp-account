2380ff324bacb1c82f807746ea83386addecdb5a
mariembencheikh <58473159+mariembencheikh@users.noreply.github.com>
2024-08-24 16:11:34 +0200

    feat: arabic view - rtl (#332)
    * feat: arabic view - rtl
    
    * fix: update to use i18nConfig object

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,29 +3,42 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
 
-import { theme } from '@graasp/ui';
+import { langs } from '@graasp/translations';
+import { ThemeProvider } from '@graasp/ui';
 
 import App from './App';
 import i18nConfig from './config/i18n';
 import {
   QueryClientProvider,
   ReactQueryDevtools,
+  hooks,
   queryClient,
 } from './config/queryClient';
 
-const Root = (): JSX.Element => (
-  <QueryClientProvider client={queryClient}>
-    <I18nextProvider i18n={i18nConfig}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ToastContainer stacked position="bottom-right" />
+const ThemeWrapper = () => {
+  const { data: currentMember } = hooks.useCurrentMember();
+
+  return (
+    <ThemeProvider
+      langs={langs}
+      languageSelectSx={{ mb: 2, mr: 2 }}
+      i18n={i18nConfig}
+      defaultDirection={i18nConfig.dir(currentMember?.extra?.lang)}
+    >
+      <CssBaseline />
+      <I18nextProvider i18n={i18nConfig}>
         <Router>
           <App />
         </Router>
-      </ThemeProvider>
-    </I18nextProvider>
+        <ToastContainer stacked position="bottom-right" />
+      </I18nextProvider>
+    </ThemeProvider>
+  );
+};
+const Root = (): JSX.Element => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeWrapper />
     {import.meta.env.DEV && <ReactQueryDevtools />}
   </QueryClientProvider>
 );
