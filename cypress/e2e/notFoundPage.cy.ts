fad3ce7471009efff18ec108b3f2dbeb6125ef51
mariembencheikh <mariembencheikh71@gmail.com>
2024-08-24 16:57:58 +0200

    fix:apply review comments

--- cypress/e2e/notFoundPage.cy.ts
+++ cypress/e2e/notFoundPage.cy.ts
@@ -1,23 +1,28 @@
+import {
+  GO_TO_HOME_LINK_ID,
+  NOT_FOUND_MESSAGE_ID,
+  NOT_FOUND_TEXT_ID,
+} from '@/config/selectors';
+
 import { BOB } from '../fixtures/members';
 
 describe('404 Page Test', () => {
   beforeEach(() => {
     cy.setUpApi({ currentMember: BOB });
+    cy.visit('/non-existing-page');
   });
   it('should display 404 page for non-existing routes', () => {
-    cy.visit('/non-existing-page');
+    cy.get(`#${NOT_FOUND_TEXT_ID}`).should('contain', 'Page not found');
 
-    cy.url().should('include', '/non-existing-page');
-
-    cy.get('h1').contains('404 Page not found').should('be.visible');
-
-    cy.contains('Sorry, we could not find this page.').should('be.visible');
-    cy.get('a').contains('Go to Home').should('have.attr', 'href', '/');
+    cy.get(`#${NOT_FOUND_MESSAGE_ID}`).should(
+      'contain',
+      'Sorry, we could not find this page.',
+    );
   });
 
   it('should navigate to the home page when clicking "Go to Home"', () => {
     cy.visit('/non-existing-page');
-    cy.get('a').contains('Go to Home').click();
+    cy.get(`#${GO_TO_HOME_LINK_ID}`).click();
 
     // Check if the URL is the home page
     cy.url().should('eq', `${Cypress.config().baseUrl}/`);
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Container, Stack, Typography } from '@mui/material';
+
 import { useAccountTranslation } from '@/config/i18n';
+import {
+  GO_TO_HOME_LINK_ID,
+  NOT_FOUND_MESSAGE_ID,
+  NOT_FOUND_TEXT_ID,
+} from '@/config/selectors';
 
 const NotFoundPage = (): JSX.Element => {
   const { t } = useAccountTranslation();
 
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>{t('NOT_FOUND_PAGE_TEXT')}</h1>
-      <p>{t('NOT_FOUND_Page_MESSAGE')}</p>
-      <Link to="/">{t('GO_TO_HOME_TEXT')}</Link>
-    </div>
+    <Container
+      sx={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Stack spacing={2} marginTop={5}>
+        <Typography variant="h1" id={NOT_FOUND_TEXT_ID}>
+          {t('NOT_FOUND_PAGE_TEXT')}
+        </Typography>
+        <Typography variant="body1" id={NOT_FOUND_MESSAGE_ID}>
+          {t('NOT_FOUND_PAGE_MESSAGE')}
+        </Typography>
+        <Link to="/" id={GO_TO_HOME_LINK_ID}>
+          {t('GO_TO_HOME_TEXT')}
+        </Link>
+      </Stack>
+    </Container>
   );
 };
 
